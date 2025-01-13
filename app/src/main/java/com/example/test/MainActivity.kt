a7e576699d06bd7e3314d0d63c148f17ec86b6c4
weixin_60498767 <weixin_60498767@noreply.gitcode.com>
2025-01-14 00:46:02 +0800

    feat:更改主入口文件和项目依赖配置

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,24 +1,50 @@
-package com.example.test;
+package com.example.test
 
-import android.os.Bundle;
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.test.R
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // 配置 WebView 设置
+        webView.settings.javaScriptEnabled = true // 启用 JavaScript
+
+        // 设置 WebViewClient 和 WebChromeClient
+        webView.webViewClient = WebViewClient() // 阻止跳转到外部浏览器
+        webView.webChromeClient = WebChromeClient()
+
+        //////
+        webView.addJavascriptInterface(object {
+            @JavascriptInterface
+            fun showToast(message: String) {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+            }
+        }, "Android")
+        /////
+        // 加载本地 HTML 文件
+        val filePath = "file:///android_asset/html/index.html"
+        webView.loadUrl(filePath)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 释放资源，避免内存泄漏
+        webView.apply {
+            clearCache(true)
+            clearHistory()
+            removeAllViews()
+            destroy()
+        }
     }
 }
